67e2acd685024fa89f43582b22f01b6de37491ce
Prateek <job.prateekjadon@gmail.com>
2024-05-09 01:00:03 +0530

    socket added

--- app.js
+++ app.js
@@ -1,45 +1,44 @@
 const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
 // Serve static files (including index.html)
 app.use(express.static("public"));
 
 const PORT = 3000;
 
-app.use(express.json());
+io.on("connection", (socket) => {
+    console.log("A user connected");
 
-app.post('/data', (req, res) => {
-    console.log(req.body); // This will log { key: 'value' }
-    res.send('Data received');
+    // Receive coordinates from the client
+    socket.on("coordinates", (data) => {
+        console.log("Received Coordinates:", data.latitude, data.longitude);
+
+        // Send coordinates to the Spring Boot server
+        // axios.post("http://<your-spring-boot-server-ip>:<port>/api/map-matching", {
+        //     latitude: data.latitude,
+        //     longitude: data.longitude
+        // })
+        // .then(response => {
+        //     console.log("Mapped Coordinates:", response.data);
+
+        //     // Send the mapped coordinates back to the client
+        //     // socket.emit("mappedCoordinates", response.data);
+        // })
+        // .catch(error => {
+        //     console.error("Error sending coordinates to Spring Boot server:", error);
+        // });
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
 });
 
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-
-// const express = require('express');
-// const http = require('http');
-// const socketIo = require('socket.io');
-
-// const app = express();
-// const server = http.createServer(app);
-// const io = socketIo(server);
-
-// const PORT = 3000;
-
-// io.on('connection', (socket) => {
-//     console.log('A client connected');
-
-//     // Assuming you have the coordinates available in latitude and longitude variables
-//     const latitude = 40.7128;
-//     const longitude = -74.0060;
-
-//     // Send coordinates to the Spring Boot server
-//     socket.emit('coordinates', { latitude, longitude });
-// });
-
-// server.listen(PORT, () => {
-//     console.log(`Server is running on http://localhost:${PORT}`);
-// });
